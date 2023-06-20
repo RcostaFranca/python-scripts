eb529ff35b3c583114d068744c4579ec6f4a995e
Renato França <renato.franca@routz.com.br>
2023-06-20 16:38:30 -0300

    add comments

--- getinfo.py
+++ getinfo.py
@@ -5,14 +5,14 @@ pd.set_option('display.max_columns', None)
 #loja = pd.read_excel('Carrefour-controle-lojas\lojas\IP TTU - Timbauba.xlsx','Plano IP')
 
 
-# retira caracteres indesejados do resultado da pesquisa xls
+# Define a função cleaning para remover caracteres indesejados de uma string
 def cleaning(var):
     var = var.replace("[","")
     var = var.replace("]","")
     var = var.replace("'","")
     return var
 
-# localiza a linha com o endereço ip e reorganiza a informação para formato de string
+# Função infoFilter localiza a linha com o endereço IP e reorganiza a informação em formato de string
 def infoFilter(xls, nome):
     filtred = xls.loc[xls['Unnamed: 0']==nome]
     subNome = str(filtred ["Unnamed: 0"].values)
@@ -20,10 +20,10 @@ def infoFilter(xls, nome):
     rede = str(filtred ["Unnamed: 1"].values)+str(filtred ["Unnamed: 2"].values)+str(filtred ["Unnamed: 3"].values)+str(filtred ["Unnamed: 4"].values)+str(filtred ["Unnamed: 5"].values)+str(filtred ["Unnamed: 6"].values)+str(filtred ["Unnamed: 7"].values)
     rede = cleaning(rede)
     resultado = f"{subNome};{rede}"
-    if resultado == ";": resultado = " ; " 
+    if resultado == ";": resultado = " ; " # Se o resultado for apenas um ponto e vírgula, substitui por " ; "
     return resultado
 
-#retorna uma lista com as subnets e gw do xls
+# Função getVlan retorna uma lista com as subnets e gateways do arquivo xls
 def getVlan( path , file, sheet):
     totalpath = f"{path}\{file}"
     raw = pd.read_excel(totalpath, sheet)
@@ -41,20 +41,13 @@ def getVlan( path , file, sheet):
 
 
 
-# loja = getVlan("python-scripts\lojas","IP BRE - BIG Avenida Recife.xlsx",'Plano IP')
-# print(loja)
-
-#path para o xls de controle
-controlxls = os.path.normpath('python-scripts\controle\Controle Geral_rtz.xlsx')
-
-#retorna o numero da linha que sera editada
+# Função getControlrow retorna o número da linha que será editada no arquivo xls
 def getControlrow(xls, sheet, sigla):
     raw = pd.read_excel(xls, sheet,)
     rowNumber = raw.loc[raw['Sigla']==str(sigla)].index[0]
     return rowNumber
 
-# print(getControlrow(controlxls,'Controle',loja[0]))
-
+# Função inputXls recebe um plano, loja como parâmetros e realiza operações no arquivo xls
 def inputXls(plan, loja):
     
     rownumber = getControlrow(plan, 'Controle', loja[0])
@@ -62,11 +55,11 @@ def inputXls(plan, loja):
     counter = 1
     writer = pd.ExcelWriter(plan, engine='openpyxl', mode='a', if_sheet_exists='overlay')
     for info in range(1, len(loja)):
-        print(counter)        
+        print(counter)
+        # Obtém a segunda parte da string dividida por ponto e vírgula        
         dfvlan10 = pd.DataFrame([str(loja[info]).split(";")[1]])
+        # Escreve o DataFrame no arquivo Excel
         dfvlan10.to_excel(writer,sheet_name='Controle', startrow=rownumber, startcol=counter, header=False, index=False)
         counter = counter + 1
         print(loja[info])
     writer.close()
-
-# inputXls(controlxls, loja)
