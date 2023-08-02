9d7e30e1afafb64ae2872388de789800c85c2d96
renato da costa franca junior <r.franca@br.routzgroup.com>
2023-08-02 16:46:33 -0300

    add scripts

---
+++ A10-scripts/configPartition.py
@@ -0,0 +1,22 @@
+import os
+
+f5_file = open(os.path.normpath("python-scripts/A10-scripts/config-parts/LB795/LB795-partition.txt"))
+a10_file = open(os.path.normpath("python-scripts/A10-scripts/result_files/LB795/A10-LB795-partition.txt"),"a")
+
+norm_file = str(f5_file.read()).split("}")
+
+# separa a string norm_file em duas e coleta os valores de partição, server e ip
+def get_partition (norm_file):
+    partition = norm_file.split("{")[0].split(" ")[2]
+    domain = norm_file.split("{")[1].split(" ")[5]
+    domain = str(domain).replace("\n", "")    
+    srv_info = [partition, domain]
+    return srv_info
+
+
+for lines in norm_file:
+    srv_data = get_partition(lines)
+    a10_server_input =f"configure\npartition {srv_data[0]} id {srv_data[1]}\nexit\n"
+    a10_file.write(a10_server_input)    
+    print(a10_server_input)
+a10_file.close()
