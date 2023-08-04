5b77d09456355a01a47ca6a4d1e0deb2dfc7b3a0
renato da costa franca junior <r.franca@br.routzgroup.com>
2023-08-04 13:31:53 -0300

    vs config script

---
+++ A10-scripts/configA10vs.py
@@ -0,0 +1,50 @@
+import os
+import re
+
+f5_file = open(os.path.normpath("python-scripts/A10-scripts/config-parts/LB794/LB794-vs.txt"))
+a10_file = open(os.path.normpath("python-scripts/A10-scripts/result_files/LB794/A10-LB794-vs.txt"),"a")
+
+norm_file = str(f5_file.read()).split("# }")
+ip_regex= "destination \/[A-Z\-]+\/\d{1,3}(?:\.\d{1,3}){3}%\d+:\d+"
+pool_regex ="pool \/[A-Z\-]+\/[a-z0-9\-._]+"
+snat_regex ="(?:pool )?\/[A-Z\-]+\/[A-Z_\-]+"
+
+def regex_find(regex , file):
+    busca = re.finditer(regex, file)
+    membros =[]
+    for match in busca:
+        total_members = match.group()
+        membros.append(total_members)
+    return membros
+
+def get_getinfo (norm_file):
+    partition_server = norm_file.split("{")[0]
+    partition = partition_server.split("/")[1]
+    virtual_server = partition_server.split("/")[2]
+    ip_port_vs = regex_find(ip_regex, norm_file)
+    ip_port = ip_port_vs[0].split("/")[2]
+    ip_port = ip_port.split("%")
+    ip = ip_port[0]
+    port = ip_port[1].split(":")[1]
+    try:
+        pool = regex_find(pool_regex, norm_file)
+        pool = pool[0].split("/")[2]
+    except:
+        pool = " "
+    try:
+        snat = regex_find(snat_regex, norm_file)
+        snat = snat[0].split("/")[2]
+        snat = f"source-nat pool {snat}"
+    except:
+        snat = "source-nat auto"
+    return [partition, virtual_server, ip, port, pool, snat]
+
+
+def write_vs (vs_list):
+    vs_info = get_getinfo(vs_list)
+    a10_vs_input = f"active-partition {vs_info[0]}\nconfigure\nslb virtual-server {vs_info[1]} {vs_info[2]}\nport {vs_info[3]} tcp\n{vs_info[5]}\nservice-group {vs_info[4]}\nexit\nexit " 
+    return a10_vs_input
+
+for itens in norm_file:
+    print(write_vs(itens))
+    
